071b87ad0c2d17398727ee55c5b70d6c3049fece
Abraham Arumbaka <102513113+abrahamarumbaka@users.noreply.github.com>
2024-08-07 17:05:44 -0700

    Update velero-deploy.sh

--- velero-deploy.sh
+++ velero-deploy.sh
@@ -12,22 +12,22 @@ if [ ! -f ~/gke-casa/velero ]; then
 fi
 
 echo "-------Create a GCS storage bucket if not exist"
-cat bucket4velero1
+cat gke-demo-back
 if [ `echo $?` -eq 1 ];then
   echo $MY_BUCKET-$(date +%d%H%M%s) > bucket4velero1
   gsutil mb gs://$(cat bucket4velero1)/
 fi
 
 echo "-------Create a service account for velero"
-gcloud iam service-accounts list | grep abrahamarumbaka
+sudo gcloud iam service-accounts list | grep abrahamarumbaka
 if [ `echo $?` -eq 1 ];then
   export MY_PROJECT_ID=$(gcloud config get-value project)
   MY_GSA_NAME=abrahamarumbaka
 
-  gcloud iam service-accounts create $MY_GSA_NAME \
+ sudo gcloud iam service-accounts create $MY_GSA_NAME \
     --display-name "Velero service account"
 
-  MY_SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list \
+  MY_SERVICE_ACCOUNT_EMAIL=$(sudo gcloud iam service-accounts list \
     --filter="displayName:Velero service account" \
     --format 'value(email)')
 
@@ -47,26 +47,26 @@ if [ `echo $?` -eq 1 ];then
     iam.serviceAccounts.signBlob
   )
 
-  gcloud iam roles list --project $MY_PROJECT_ID | grep Velero
+  sudo gcloud iam roles list --project $MY_PROJECT_ID | grep Velero
   if [ `echo $?` -eq 1 ];then
-    gcloud iam roles create velero.server \
+    sudo gcloud iam roles create velero.server \
       --project $MY_PROJECT_ID \
       --title "Velero Server" \
       --permissions "$(IFS=","; echo "${ROLE_PERMISSIONS[*]}")"
   fi
 
-  gcloud projects add-iam-policy-binding $MY_PROJECT_ID \
+  sudo gcloud projects add-iam-policy-binding $MY_PROJECT_ID \
     --member serviceAccount:$MY_SERVICE_ACCOUNT_EMAIL \
     --role projects/$MY_PROJECT_ID/roles/velero.server
 
-  gsutil iam ch serviceAccount:$MY_SERVICE_ACCOUNT_EMAIL:objectAdmin gs://$(cat bucket4velero1)
+  sudo gsutil iam ch serviceAccount:$MY_SERVICE_ACCOUNT_EMAIL:objectAdmin gs://$(cat bucket4velero1)
 
-  gcloud iam service-accounts keys create abrahamsa4velero1 \
+  sudo gcloud iam service-accounts keys create abrahamsa4velero1 \
     --iam-account $MY_SERVICE_ACCOUNT_EMAIL
 fi
 
 echo "-------Install velero using the SA"
-velero install \
+sudo velero install \
     --provider gcp \
     --plugins velero/velero-plugin-for-gcp:v1.6.0 \
     --bucket $(cat bucket4velero1) \
@@ -78,11 +78,11 @@ velero install \
 #     --plugins velero/velero-plugin-for-gcp:v1.6.0,velero/velero-plugin-for-csi:v0.3.0 \
 
 echo "-------One time On-Demand Backup of yong-postgresql namespace"
-kubectl wait --for=condition=ready --timeout=180s -n velero pod -l component=velero
-velero backup create yong-postgresql-backup --include-namespaces yong-postgresql
+sudo kubectl wait --for=condition=ready --timeout=180s -n velero pod -l component=velero
+sudo velero backup create yong-postgresql-backup --include-namespaces yong-postgresql
 
 echo "-------Hourly scheduled backup of yong-postgresql namespace"
-kubectl create -f velero-schedule.yaml
+sudo kubectl create -f velero-schedule.yaml
 
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
